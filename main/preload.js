50a2e808b2d0244da47daf4c5c3f271ed827a68a
Inbestigator <119569726+Inbestigator@users.noreply.github.com>
2024-04-19 07:28:44 -0700

    Delete main directory

--- main/preload.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/* eslint-disable @typescript-eslint/no-namespace */
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const electron_1 = require("electron");
-// We are using the context bridge to securely expose NodeAPIs.
-// Please note that many Node APIs grant access to local system resources.
-// Be very cautious about which globals and APIs you expose to untrusted remote content.
-electron_1.contextBridge.exposeInMainWorld("electron", {});
